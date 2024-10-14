037dfcbcc93d3ff12313d0b674be7b6ba7c4bc43
ishika2201 <gandhi.ishika22@gmail.com>
2024-10-14 03:14:32 -0400

    WebSocket Established

---
+++ src/main/java/com/ika/chat/ChatApplication.java
@@ -0,0 +1,13 @@
+package com.ika.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/ika/chat/chat/MessageType.java
@@ -0,0 +1,8 @@
+package com.ika.chat.chat;
+
+public enum MessageType {
+
+    CHAT,
+    JOIN,
+    LEAVE
+}
